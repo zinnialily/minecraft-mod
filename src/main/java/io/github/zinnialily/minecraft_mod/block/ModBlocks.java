b34e08cb17e40fe8be907f5cd2757bdecf81bb92
zinnialily <zinnialily303@gmail.com>
2026-06-25 23:18:18 -0500

    attempted to make a new cabbage block spawn when one was collected [fail]

--- src/main/java/io/github/zinnialily/minecraft_mod/block/ModBlocks.java
+++ src/main/java/io/github/zinnialily/minecraft_mod/block/ModBlocks.java
@@ -10,19 +10,53 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-
+import java.util.Random;
 public class ModBlocks {
-    public static final Block CABBAGE_BLOCK = register(
+    /*public static final Block CABBAGE_BLOCK = register(
             "cabbage",
             Block::new,
             //need to change
             BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS)
                     .strength(4.0f)
                     .requiresCorrectToolForDrops()
+                    .sound(SoundType.WOOD),
+            true
+    );*/
+    public static final Block CABBAGE_BLOCK = register(
+            "cabbage",
+            (properties) -> new Block(properties) {
+                @Override
+                public InteractionResult useWithoutItem(BlockState blockState, Level level, BlockPos blockPos, Player player, BlockHitResult blockHitResult) {
+                    if (!level.isClientSide()) {
+                        level.destroyBlock(blockPos, false);
+
+                        int offsetX = level.getRandom().nextInt(3) - 1;
+                        int offsetY = level.getRandom().nextInt(3) - 1;
+                        int offsetZ = level.getRandom().nextInt(3) - 1;
+
+                        BlockPos newPos = blockPos.offset(offsetX, offsetY, offsetZ);
+
+                        if (level.getBlockState(newPos).isAir()) {
+                            level.setBlock(newPos, this.defaultBlockState(), 3);
+                        }
+
+                        return InteractionResult.SUCCESS;
+                    }
+                    return InteractionResult.PASS;
+                }
+            },
+            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS)
+                    .strength(0.5f)
                     .sound(SoundType.WOOD),
             true
     );
